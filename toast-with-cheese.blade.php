2c314bf6c777c9fd910dd8c3f1031d8178311efa
novecentonove <53484559+novecentonove@users.noreply.github.com>
2020-12-23 18:38:57 +0100

    Update toast-with-cheese.blade.php

--- toast-with-cheese.blade.php
+++ toast-with-cheese.blade.php
@@ -1,18 +1,20 @@
-    @if ($message = session()->get('info'))
-    <div id="snackbar"  class="bg-gray-500 py-6 text-center">
-        <button type="button" class="close" data-dismiss="alert">×</button>
-        <strong>{{ $message }}</strong>
-    </div>
-    @endif
-    
-    
-    @push('scripts')
-    <script>
-        (function myFunction() {
-          var x = document.getElementById("snackbar");
-          x.className = "show";
-          setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
-        })()
-        console.log('script');
-    </script>
-    @endpush
+@if ($message = session()->get('info'))
+<div id="toastwithcheese"  class="bg-gray-500 py-6 text-center">
+    <button type="button" class="close" data-dismiss="alert">×</button>
+    <strong>{{ $message }}</strong>
+</div>
+@endif
+
+
+@push('scripts')
+<script>
+    (function toast() {
+      setTimeout(() => {
+        const toast = document.getElementById("toastwithcheese");
+        toast.className = "show";
+        setTimeout(function(){ toast.className = toast.className.replace("show", ""); }, 3000);
+      }, 500);
+    })()
+    console.log('script');
+</script>
+@endpush
